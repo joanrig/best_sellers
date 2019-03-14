2fbe712a3f93403f40ecb5ce4c6bcd0f2f23ad13
joan <joanrigdon@gmail.com>
2019-03-14 01:07:38 -0400

    first commit

---
+++ lib/best_sellers.rb
@@ -0,0 +1,6 @@
+require "best_sellers/version"
+
+module BestSellers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/best_sellers_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BestSellers do
+  it "has a version number" do
+    expect(BestSellers::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
